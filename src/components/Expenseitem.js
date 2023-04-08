a238cc4dbbd0935b1f3b2bca5645901f3e679fcf
ibad81 <ibadbaig921@gmail.com>
2023-04-08 07:08:46 -0700

    vd15

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,5 +1,10 @@
 import './Expenseitem.css'
 function Expenseitem(props){
+
+    const month=props.date.toLocaleString('en-US',{month:'long'});
+    const Day=props.date.toLocaleString('en-US',{day:'2-digit'});
+    const Year=props.date.getFullYear('en-US',{});
+
     // return <h2>Expense item</h2>;
 
     // const expenseDate=new Date(2021,2,28);
@@ -16,7 +21,9 @@ function Expenseitem(props){
     // </div>
     <div className="expense-item">
     <div>
-        <div>{props.date.toISOString}</div>
+        <div>{month}</div>
+        <div>{Year}</div>
+        <div>{Day}</div>
     </div>
     <div className='expense-item__description'>
     <h2>{props.title}</h2>
