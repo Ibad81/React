3caf37e87b6e5c8a57bf52af96d0246372de3610
ibad81 <ibadbaig921@gmail.com>
2023-04-08 07:31:41 -0700

    vd 16 ExpenseData

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,11 +1,9 @@
+import ExpenseDate from './ExpenseDate';
 import './Expenseitem.css'
 function Expenseitem(props){
 
-    const month=props.date.toLocaleString('en-US',{month:'long'});
-    const Day=props.date.toLocaleString('en-US',{day:'2-digit'});
-    const Year=props.date.getFullYear('en-US',{});
-
-    // return <h2>Expense item</h2>;
+    
+ // return <h2>Expense item</h2>;
 
     // const expenseDate=new Date(2021,2,28);
     // const expenseTile='Car Insurance';
@@ -20,11 +18,7 @@ function Expenseitem(props){
     // </div>
     // </div>
     <div className="expense-item">
-    <div>
-        <div>{month}</div>
-        <div>{Year}</div>
-        <div>{Day}</div>
-    </div>
+    <ExpenseDate date={props.date}/>
     <div className='expense-item__description'>
     <h2>{props.title}</h2>
     <div className='expense-item__price'>${props.amount}</div>
